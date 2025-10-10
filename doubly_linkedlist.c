dd6d36e2521f300f8ba877ea1d7a980b43bd15b0
Mir Aayat <miraayat2025@gmail.com>
2025-10-10 17:18:36 +0530

    Operations on a doubly linked list

--- doubly_linkedlist.c
+++ doubly_linkedlist.c
@@ -12,8 +12,9 @@ node *linkedlist(node *header, int size);//Creating a doubly linked list
 void fwd_traversal(node *header);//display the data 
 void bwd_traversal(node *header);//display the data in reverse order
 void freelist(node *header);//free list
-void insertion(node **header);
-/*todo: insertion operations*/
+void insertion(node **header);//insertion at any location
+void deletion(node **header);//deletion from any location
+int length(node *header);//calculate length of linkedlist
 
 int main(void)
 {
@@ -28,8 +29,10 @@ int main(void)
     bwd_traversal(header);
 
     insertion(&header);
-    fwd_traversal(header);
-    bwd_traversal(header);
+
+    deletion(&header);
+
+    length(header);
 
     freelist(header);
     header = NULL;
@@ -74,6 +77,7 @@ void fwd_traversal(node *header)
     if (header == NULL)
     {
         printf("List is empty\n");
+        return;
     }
     node *temp = header;
     printf("Data: ");
@@ -90,6 +94,7 @@ void bwd_traversal(node *header)
     if (header == NULL)
     {
         printf("List is empty\n");
+        return;
     }
     node *temp = header;
 
@@ -136,13 +141,14 @@ void insertion(node **header)
         newnode->next = NULL;
         newnode->prev = NULL;
     }
-
+    
     node *ptr = *header;
 
     if (pos == 1) /*insertion at beginning*/
     {
         *header = newnode;
         newnode->next = ptr;
+        if(ptr != NULL)
         ptr->prev = newnode;
     }
     else // insertion at any position or at last
@@ -158,11 +164,77 @@ void insertion(node **header)
             free(newnode);
             return;
         }
-        
+
         newnode->next = ptr->next;//if at end ptr->next points to null
         if(ptr->next != NULL)
         ptr->next->prev = newnode;
         ptr->next = newnode;
         newnode->prev  = ptr;
     }
+    fwd_traversal(*header);//display list
+}
+
+void deletion (node **header)
+{
+    int pos;
+
+    printf("Enter the node to be deleted: ");
+    scanf("%i",&pos);
+
+    if(*header == NULL)
+    {
+        printf("List is empty!\n");
+        return;
+    }
+
+    node *temp = *header;
+    node *next = *header;
+
+    if(pos == 1)
+    {
+        temp = temp->next;
+        if(temp != NULL)
+        temp->prev = NULL;
+        free(*header);
+        *header = temp;
+    }
+    else{
+        for(int i = 1; i < pos-1 && temp != NULL; i++)
+        {
+            temp = temp->next;
+        }
+        for(int i = 1; i < pos && next != NULL; i++)
+        {
+            next = next->next;
+        }
+        if(temp == NULL || next == NULL)
+        {
+            printf("Position out of bounds!!");
+            return;
+        }
+        temp->next = next->next;
+        if(next->next != NULL)
+        {
+            next->next->prev = temp;
+        }
+        free(next);
+    }
+    fwd_traversal(*header);
+}
+int length(node *header)
+{
+    node * temp = header;
+
+    if(header == NULL){
+        printf("length is: 0");
+        return 1;
+    }
+
+    int count = 1;
+
+    for(int i = 0; temp->next != NULL; temp = temp->next)
+    {
+        count++;
+    }
+    printf("length of linkedlist: %i", count);
 }
